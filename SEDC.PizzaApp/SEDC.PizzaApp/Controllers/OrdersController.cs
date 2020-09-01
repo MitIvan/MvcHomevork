88422e243ea72fc6af44bf5295fdc5575d71698f
Ivan Mitev <ivan.mitev90@gmail.com>
2020-09-01 21:52:26 +0200

    HomeWork_1

---
+++ SEDC.PizzaApp/SEDC.PizzaApp/Controllers/OrdersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if(id != null)
+            {
+                Order order = OrdersDb.Orders.FirstOrDefault(o => o.Id == id);
+                return View(order);
+            }
+            else
+            {
+                return new EmptyResult();
+            }
+        }
+
+        public IActionResult JsonData()
+        {
+            Order order = OrdersDb.Orders[0];
+
+            return new JsonResult(order);
+        }
+
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ SEDC.PizzaApp/SEDC.PizzaApp/OrdersDb.cs
@@ -0,0 +1,29 @@
+﻿using SEDC.PizzaApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp
+{
+    public static class OrdersDb
+    {
+        public static List<Order> Orders = new List<Order>
+        {
+            new Order()
+            {
+                Id = 1,
+                PizzaType = "Capri",
+                Price = 300
+                
+            },         
+            new Order()
+            {
+                Id = 2,
+                PizzaType = "Margarita",
+                Price = 250
+                
+            }
+        };
+    }
+}
